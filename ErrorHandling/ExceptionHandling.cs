c44bdf1e5e3d49c1cf13de1382b013199d7f5d02
Daniel Kalotay <daniel.kalotay@7digital.com>
2015-01-06 14:09:40 +0000

    test exception handling

---
+++ ErrorHandling/ExceptionHandling.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ErrorHandling
+{
+	[TestFixture]
+	public class ExceptionHandling
+	{
+		public class CustomException: Exception
+		{
+			public CustomException(Exception exception):base("custom", exception)
+			{}
+
+			public CustomException()
+			{}
+		}
+
+		public class DerivedCustomException: CustomException
+		{}
+
+		[Test]
+		public void CatchAll()
+		{
+			try
+			{
+				Throw();
+			}
+			catch
+			{
+			}
+		}
+
+		[Test]
+		public void CatchParent()
+		{
+			try
+			{
+				Throw();
+			}
+			catch(Exception e)
+			{
+				Assert.That(e, Is.InstanceOf<CustomException>());
+			}
+		}
+
+		[Test]
+		public void CatchExact()
+		{
+			try
+			{
+				Throw();
+			}
+			catch(CustomException e)
+			{
+				Assert.That(e, Is.InstanceOf<CustomException>());
+			}
+		}
+
+		[Test]
+		[ExpectedException(typeof(CustomException))]
+		public void CatchDerived()
+		{
+			try
+			{
+				Throw();
+			}
+			catch(DerivedCustomException e)
+			{
+				Assert.That(e, Is.InstanceOf<CustomException>());
+			}
+		}
+
+		[Test]
+		public void MultiCatch()
+		{
+			var catchCount = 0;
+			try
+			{
+				Throw();
+			}
+			catch (CustomException)
+			{
+				catchCount += 1;
+			}
+			catch (Exception)
+			{
+				catchCount += 1;
+			}
+			Assert.That(catchCount, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void StackPreservation()
+		{
+			var exception = Assert.Catch<CustomException>(PreserveStack);
+			Assert.That(exception.StackTrace, Is.StringContaining("Throw()"));
+		}
+
+		[Test]
+		public void StackLoss()
+		{
+			var exception = Assert.Catch<CustomException>(LoseStack);
+			Assert.That(exception.StackTrace, Is.Not.StringContaining("Throw()"));
+		}
+
+		[Test]
+		public void StackWrapping()
+		{
+			var exception = Assert.Catch<CustomException>(WrapStack);
+			Assert.That(exception.StackTrace, Is.Not.StringContaining("Throw()"));
+			Assert.That(exception.InnerException.StackTrace, Is.StringContaining("Throw()"));
+		}
+
+		private void WrapStack()
+		{
+			try
+			{
+				Throw();
+			}
+			catch (CustomException e)
+			{
+				throw new CustomException(e);
+			}
+		}
+
+		private void LoseStack()
+		{
+			try
+			{
+				Throw();
+			}
+			catch (CustomException e)
+			{
+				throw e;
+			}
+		}
+
+		private void PreserveStack()
+		{
+			try
+			{
+				Throw();
+			}
+			catch (CustomException)
+			{
+				throw;
+			}
+		}
+
+		private static void Throw()
+		{
+			throw new CustomException();
+		}
+	}
+}
