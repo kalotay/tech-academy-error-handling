f1483a048e53337f6b7cfc8d89e39fd0965603a6
Daniel Kalotay <daniel.kalotay@7digital.com>
2015-01-06 15:06:47 +0000

    sevendigital api call example

---
+++ ErrorHandling/SevendigitalApiCall.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using SevenDigital.Api.Schema.Releases;
+using SevenDigital.Api.Wrapper;
+
+namespace ErrorHandling
+{
+	[TestFixture]
+	public class SevendigitalApiCall
+	{
+		[Test, Ignore]
+		public async Task SearchesReleases()
+		{
+			var api = Api<ReleaseSearch>.Create.WithQuery("the");
+			var searchResult = await api.Please();
+			Assert.That(searchResult.Results.Count, Is.GreaterThan(0));
+			Assert.That(searchResult.Results[0].Release.Title, Is.StringContaining("the"));
+		}
+	}
+}
