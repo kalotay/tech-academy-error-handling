80f6cc3c0d7c3cc52bdaa99c93f3d167617a6fd2
Daniel Kalotay <daniel.kalotay@7digital.com>
2015-01-06 11:50:24 +0000

    iterative square root example

---
+++ ErrorHandling/IterativeSquareRoot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ErrorHandling
+{
+	[TestFixture]
+	public class IterativeSquareRoot
+	{
+		public static double SquareRoot(double x, double guess = 1, int iterations = 10)
+		{
+			var estimate = guess;
+			for (var i = 0; i < iterations; i++)
+			{
+				estimate = 0.5*(estimate + x/estimate);
+			}
+			return estimate;
+		}
+
+		[TestCase(0)]
+		[TestCase(1)]
+		[TestCase(4)]
+		[TestCase(2)]
+		[TestCase(0.5)]
+		public void IterativeMethodMatchesExact(double x)
+		{
+			var expected = Math.Sqrt(x);
+			var actual = SquareRoot(x);
+			Assert.That(actual, Is.EqualTo(expected).Within(0.01));
+		}
+	}
+}
